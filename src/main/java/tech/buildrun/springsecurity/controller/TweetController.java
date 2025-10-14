9ec2c5d9d3d85a3cdf52804c20adf386109edb4f
Yasmin Santos <yasminsantos.eng@gmail.com>
2025-10-14 17:29:46 -0300

    feat: Funcionalidade de criar Tweet

---
+++ src/main/java/tech/buildrun/springsecurity/controller/TweetController.java
@@ -0,0 +1,39 @@
+package tech.buildrun.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import tech.buildrun.springsecurity.repository.TweetRepository;
+import tech.buildrun.springsecurity.repository.UserRepository;
+import tech.buildrun.springsecurity.controller.dto.CreateTweetDto;
+import tech.buildrun.springsecurity.entities.Tweet;
+
+import java.util.UUID;
+
+@RestController
+public class TweetController {
+
+    private final TweetRepository tweetRepository;
+    private final UserRepository userRepository;
+
+    public TweetController(TweetRepository tweetRepository, UserRepository userRepository) {
+        this.tweetRepository = tweetRepository;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/tweets")
+    public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto dto,
+                                            JwtAuthenticationToken token) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var tweet = new Tweet();
+        tweet.setUser(user.get());
+        tweet.setContent(dto.content());
+
+        tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+}
