1fa8d1593fb31b0a9fa1e00754842529aa6951bb
Yasmin Santos <yasminsantos.eng@gmail.com>
2025-10-20 17:32:13 -0300

    feat: componente cabeçalho

--- frontend/PROJETO-JWT/src/app/app.component.ts
+++ frontend/PROJETO-JWT/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
